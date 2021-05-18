2df3793f482389823d9ab73832018897521ef4d7
astercraker <venus_bronce@hotmail.com>
2021-05-18 17:18:19 -0500

    Added code to jpg support

--- main.cpp
+++ main.cpp
@@ -17,12 +17,12 @@ struct Color
 };
 class ConvertImageToArray{
     public:
-        vector<vector<Color>> Procesing(CImg<unsigned char> src){
+        vector<vector< vector<int> >> Procesing(CImg<unsigned char> src){
             int width = src.width();
             int height = src.height();
             int canales = src.spectrum();
 
-            vector<vector<Color>> rgbs_values = vector<vector<Color>> (width, vector<Color>(height));;
+            vector<vector< vector<int> >> color_values = vector<vector< vector<int> >> (width, vector<vector<int> >(height));
             cout << width << "x" << height << " canales: " << canales << endl;
             int counter = 0;
             ofstream MyFile("rgb_values.txt");
@@ -30,14 +30,19 @@ class ConvertImageToArray{
             for (int r = 0; r < height; r++){
                 for (int c = 0; c < width; c++){
                     counter += 1;
-                    Color color = {(int)src(c,r,0,0), (int)src(c,r,0,1), (int)src(c,r,0,2), (int)src(c,r,0,3)};
-                    rgbs_values[r][c] = color;
+                    if(canales == 4){
+                        color_values[r][c] = {(int)src(c,r,0,0), (int)src(c,r,0,1), (int)src(c,r,0,2), (int)src(c,r,0,3)};
+                    }
+                    else if(canales == 3){
+                        color_values[r][c] = {(int)src(c,r,0,0), (int)src(c,r,0,1), (int)src(c,r,0,2)};
+                    }
+
                     buffer << "counter: " << counter << " (" << r << "," << c << ") =" << " R: " << (int)src(c,r,0,0) << " G: " << (int)src(c,r,0,1) << " B: " << (int)src(c,r,0,2) << " A: " << (int)src(c,r,0,3) << endl;
                 }
             }
             MyFile << buffer.str();
             MyFile.close();
-            return rgbs_values;
+            return color_values;
         };
 };
 
@@ -81,16 +86,17 @@ ChangesVector::ChangesVector(float _V, float _lenVect){
 int main()
 {
 
-    string filename = "sword_16x16.png";
+    string filename = "sword_16x16.jpg";
     CImg<unsigned char> src(filename.c_str());
 
     ConvertImageToArray converted;
-    vector<vector<Color>> vect = converted.Procesing(src);
+    vector<vector< vector<int> >> vect = converted.Procesing(src);
+    cout << "Pixel Size: " << vect[0][0].size() << endl;
     for (int i = 0; i < vect.size(); i++)
     {
         for (int j = 0; j < vect[i].size(); j++)
         {
-            cout << vect[i][j].blue << " ";
+            cout << vect[i][j][0] << " ";
         }
         cout << endl;
     }
